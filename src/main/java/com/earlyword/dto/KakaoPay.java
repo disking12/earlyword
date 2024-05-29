7880642b26334114e2bf7b13c5c74d8ac0721a94
DESKTOP-OVCFM7K\jylim <jylim@itsconv.com>
2024-05-29 17:33:14 +0900

    feat: 카카오 결제 승인 DTO  개발

--- src/main/java/com/earlyword/dto/KakaoPay.java
+++ src/main/java/com/earlyword/dto/KakaoPay.java
@@ -30,4 +30,41 @@ public class KakaoPay {
 		private String vat_amount;
 		private String tax_free_amount;
 	}
+
+	@Getter	@Setter	@ToString
+	public static class ApproveResponse {
+		private String aid;
+		private String tid;
+		private String cid;
+		private String sid;
+		private String partner_order_id;
+		private String partner_user_id;
+		private String payment_method_type;
+		private Amount amount;
+		private String item_name;
+		private String item_code;
+		private int quantity;
+		private String created_at;
+		private String approved_at;
+		private String payload;
+	}
+
+	@Getter	@Setter	@ToString
+	public static class ApproveRequest {
+		private String cid;
+		private String tid;
+		private String partner_order_id;
+		private String partner_user_id;
+		private String pg_token;
+	}
+
+	@Getter	@Setter	@ToString
+	public static class Amount {
+		private int total;
+		private int tax_free;
+		private int tax;
+		private int point;
+		private int discount;
+		private int green_deposit;
+	}
 }
