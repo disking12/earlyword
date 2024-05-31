34d73ec91f902d73eb82cd0accf6a803bbf2b0a3
DESKTOP-OVCFM7K\jylim <jylim@itsconv.com>
2024-05-31 17:38:38 +0900

    feat: 카카오 payment -> 결제 준비 요청 컨트롤러 개발

--- src/main/java/com/earlyword/KakaoPayController.java
+++ src/main/java/com/earlyword/KakaoPayController.java
@@ -1,16 +1,31 @@
 package com.earlyword;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.earlyword.dto.KakaoPay;
+import com.earlyword.service.KakaoPayService;
+
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/payment")
+@RequiredArgsConstructor
 public class KakaoPayController {
+
+	private final KakaoPayService kakaoPayService;
 
 	@GetMapping("/test")
 	public String test() {
 		return "test 데이터입니다";
+	}
+
+	@PostMapping("/ready")
+	public void readyKakaoPay(KakaoPay.ReadyRequest params) {
+		System.out.println("params = " + params);
+		kakaoPayService.readyKakaoPay(params);
 	}
 
 }
