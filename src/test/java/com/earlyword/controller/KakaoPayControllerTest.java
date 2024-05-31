d8f95546713f45f88a7c1bb08c360554fba313f2
jylim98 <jylim@itsconv.com>
2024-05-31 21:17:43 +0900

    test: 카카오 payment -> 카카오 결제준비 컨트롤러 테스트

--- src/test/java/com/earlyword/controller/KakaoPayControllerTest.java
+++ src/test/java/com/earlyword/controller/KakaoPayControllerTest.java
@@ -1,23 +1,21 @@
 package com.earlyword.controller;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
-import com.earlyword.controller.KakaoPayController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-import com.earlyword.service.KakaoPayService;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@WebMvcTest(KakaoPayController.class)
+@SpringBootTest
+@AutoConfigureMockMvc
 class KakaoPayControllerTest {
-
-	@MockBean
-	KakaoPayService kakaoPayService;
 
 	@Autowired
 	MockMvc mockMvc;
@@ -31,16 +29,19 @@ class KakaoPayControllerTest {
 
 	@Test
 	void 카카오_결제준비_컨트롤러() throws Exception {
-		mockMvc.perform(post("/payment/ready")
-			.param("item_name", "매일 영어 단어(기본)")
-			.param("quantity", "1")
-			.param("total_amount", "10000")
-			.param("vat_amount", "1000")
-			.param("partner_user_id", "test")
-			.param("partner_order_id", "test")
-			.param("tax_free_amount", "0"))
-			.andExpect(status().isOk())
-			.andDo(print());
+		ResultActions resultActions = mockMvc.perform(post("/payment/ready")
+				.param("item_name", "매일 영어 단어(기본)")
+				.param("quantity", "1")
+				.param("total_amount", "10000")
+				.param("vat_amount", "1000")
+				.param("partner_user_id", "test")
+				.param("partner_order_id", "test")
+				.param("tax_free_amount", "0")
+				.contentType(MediaType.APPLICATION_FORM_URLENCODED));
+
+
+		resultActions.andExpect(status().isOk())
+				.andDo(print());
 	}
 
 }
